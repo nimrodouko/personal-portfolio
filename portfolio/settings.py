2784d83c19ad51a3b0383e437d771a30d22bb6cd
nimrodouko <oukonimrod@gmail.com>
2025-01-04 07:38:25 +0000

    project done

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
+#https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
 
